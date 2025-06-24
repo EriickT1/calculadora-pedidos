138e660965151636250a5876dced9122df531cf7
EriickT1 <eriick1324@gmail.com>
2025-06-23 22:19:52 -0400

    Proyecto base, compila  hace test correctamente

--- src/test/java/com/ejemplo/PedidoServiceTest.java
+++ src/test/java/com/ejemplo/PedidoServiceTest.java
@@ -4,29 +4,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PedidoServiceTest {
-    PedidoService service = new PedidoService();
 
-    @Test
-    void testSinDescuentoYEnvioNormal() {
-        double total = service.calcularTotal(100, false, false);
-        assertEquals(110.0, total);
-    }
+ DescuentoRepository repo = new DescuentoRepository();
+ PedidoService service = new PedidoService(repo);
 
-    @Test
-    void testConDescuentoYEnvioExpress() {
-        double total = service.calcularTotal(100, true, true);
-        assertEquals(110.0, total); // 100 - 10% + 20
-    }
+ @Test
+ void testSinDescuentoYEnvioNormal() {
+   double total = service.calcularTotal(100, "NINGUNO", false);
+   assertEquals(110.0, total);
+ }
 
-    @Test
-    void testConDescuentoYEnvioNormal() {
-        double total = service.calcularTotal(200, true, false);
-        assertEquals(190.0, total);
-    }
+ @Test
+ void testConDescuentoYEnvioExpress() {
+   double total = service.calcularTotal(100, "PROMO10", true);
+   assertEquals(110.0, total); // 100 - 10% + 20
+ }
 
-    @Test
-    void testSinDescuentoYEnvioExpress() {
-        double total = service.calcularTotal(150, false, true);
-        assertEquals(170.0, total);
-    }
+ @Test
+ void testConDescuentoYEnvioNormal() {
+   double total = service.calcularTotal(200, "PROMO10", false);
+   assertEquals(190.0, total);
+ }
+
+ @Test
+ void testSinDescuentoYEnvioExpress() {
+   double total = service.calcularTotal(150, "NINGUNO", true);
+   assertEquals(170.0, total);
+ }
 }
